8a65a96bb2f6adae752595d8cf58361406efcfec
vadim <symphatixe@gmail.com>
2026-05-24 12:40:18 -0400

    Reworked app to use pouchdb instead of pouchdb-browser

---
+++ app/utils/utils.js
@@ -0,0 +1,8 @@
+function generateId(prefix) {
+	// Using timestamp ensures documents are sorted chronologically by allDocs()
+	return `${prefix}::${new Date().toISOString()}`;
+}
+
+function now() {
+	return new Date().toISOString();
+}
